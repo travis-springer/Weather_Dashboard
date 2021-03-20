497ac2d22a808dadcb3e055c25c486b3d4c579cd
Travis Springer <travis.springer@gmail.com>
2021-03-20 14:39:58 -0500

    Added current weather forecast

--- script.js
+++ script.js
@@ -1,20 +1,59 @@
-//var searchCity = document.getElementById("search_city");
-// var searchButton = document.getElementById("search_button");
-var searchHistory = document.getElementById("search_history");
-//var searchClear = document.getElementById("search_clear");
-var cityName = document.getElementById("city_name");
-var cityImg = document.getElementById("city_img");
-var cityTemperature = document.getElementById("city_temperature");
-var cityHumidity = document.getElementById("city_humidity");
-var cityWind = document.getElementById("city_wind");
-var cityUV = document.getElementById("city_UV");
-
 var searchStorage = [];
 
 //API Key from openweathermap.com
 var APIKey = "acd65b4cc0421391f7342feb506c6e96";
 
+function getWeather(cityName) {
+    var URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}`;
+    axios.get(URL)
+    .then(function(response) {
+        console.log(response);
+        var currentDate = new Date(response.data.dt*1000).toDateString();
+        $("#cityName").text(`${response.data.name} - ${currentDate}`);
+        var weatherIMG = response.data.weather[0].icon;
+        $("#cityImg")
+            .attr({"src": `https://openweathermap.org/img/wn/${weatherIMG}@2x.png`})
+            .attr({"alt": response.data.weather[0].description});
+        $("#cityTemp").text(`Temperature: ${kel2Far(response.data.main.temp)} F`);
+        $("#cityHumidity").text(`Humidity: ${response.data.main.humidity}%`);
+        $("#cityWind").text(`Wind: ${response.data.wind.speed} MPH`);
+        var lat = response.data.coord.lat;
+        var lon = response.data.coord.lon;
+        var URL2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+        axios.get(URL2)
+        .then (function(response) {
+            console.log(response);
+            var uvIndex = response.data.current.uvi;
+            $("#cityUV").text(`UV Index: `);
+            $("#spanUV").text(uvIndex);
+            if (uvIndex <= 2) {
+                $("#spanUV").attr({"class": "badge badge-success"})
+            } else if (uvIndex > 2 && uvIndex <= 4) {
+                $("#spanUV").attr({"class": "badge badge-primary"})
+            } else if (uvIndex > 4 && uvIndex <= 6) {
+                $("#spanUV").attr({"class": "badge badge-warning"})
+            } else {
+                $("#spanUV").attr({"class": "badge badge-danger"})
+            } 
+            //Create forecast
+            var fCasts = document.querySelectorAll(".forecast");
+            var i = 1;
+            console.log(fCasts);
+            fCasts.forEach(function () {
+                var fcastDate = new Date(response.data.daily[i].dt*1000).toDateString();
+                console.log(fcastDate);
+                var fCastDateEl = document.createElement("p");
+                fCastDateEl.innterHTML = fcastDate;
+                fCasts[i].append(fCastDateEl);
+                i++;
+            })
+        })
+        })
+    }
 
+function kel2Far (kel) {
+    return Math.floor((kel-273.15)*1.8+32);
+}
 
 function showHistory() {
     if (localStorage.getItem("Search History") == null) {
@@ -32,7 +71,8 @@ function showHistory() {
 //Search Button
 $("#searchButton").on("click", function () {
     var searchCity = $("#searchCity").val();
-    searchStorage.push(searchCity);
+    getWeather(searchCity);
+    searchStorage.unshift(searchCity);
     localStorage.setItem("Search History", JSON.stringify(searchStorage));
     showHistory();
 })
@@ -40,7 +80,7 @@ $("#searchButton").on("click", function () {
 //Clear Search History Button
 $("#searchClear").on("click", function () {
     localStorage.removeItem("Search History");
-    showHistory();
+    location.reload();
 })
 
 showHistory();
